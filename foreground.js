b31af5c7bbdeec7730d7d882fdb79e8fa8f053f6
Mr Salsa <intenso524@gmail.com>
2023-05-30 03:54:01 +0200

    chat body detector

--- foreground.js
+++ foreground.js
@@ -3,33 +3,68 @@
 // (see "content_script" key).
 // Several foreground scripts can be declared
 // and injected into the same or different pages.
-
 console.log("This prints to the console of the page (injected only if the page url matched)")
-
 
 // Content script
 
+
+//TODO: change query selector to xpath || MERGE THIS FUNCTON WITH CHATBODYCONTAINER DETETOR FUNCTION
+/***********************************
+
+        GET LAST MESSAGE
+
+***********************************/
 // Function to download the last message
 function downloadLastMessage() {
   // Retrieve the chat container element
   let chatContainer = document.querySelector("#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.relative.flex.h-full.max-w-full.flex-1.overflow-hidden > div > main > div.flex-1.overflow-hidden > div > div > div");
-
   // Retrieve the index of the last message
   let lastMessageIndex = chatContainer.childNodes.length - 2;
-
   // Retrieve the last message element
   let lastMessage = chatContainer.childNodes[lastMessageIndex];
-
   // Extract the message content
   let messageContent = lastMessage.textContent;
-
   // Perform the download action here with the extracted message content
   console.log("Download Last Message:", messageContent);
 }
-
 // Listen for messages from the popup script
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "initiateDownload") {
     downloadLastMessage();
   }
 });
+
+
+
+/***********************************
+
+        CHAT BODY CONTAINER
+              DETECTOR
+
+***********************************/
+// Create a new MutationObserver instance
+const observer = new MutationObserver((mutationsList, observer) => {
+  //chat body container
+  const chatBodyContainerClasses = 'flex flex-col text-sm dark:bg-gray-800';
+  const chatBodyContainerXPath = '//*[@id="__next"]/div[2]/div[2]/div/main/div[2]/div/div/div';
+  // Evaluate the XPath expression on the document
+  const result = document.evaluate(chatBodyContainerXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+
+  // Check if an element is found
+  if (result.singleNodeValue) {
+    console.log('Element exists!');
+    let chatBodyContainer = result.singleNodeValue;
+    // Disconnect the observer once the element is rendered
+    observer.disconnect();
+    console.log(chatBodyContainer.childNodes.length)
+  }
+});
+
+// Start observing mutations on the document body
+observer.observe(document.body, { childList: true, subtree: true });
+
+
+
+
+
+
