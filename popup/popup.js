61e40146f9a36764c850a4c666aebf6740889f6c
Mr Salsa <intenso524@gmail.com>
2023-06-07 06:31:34 +0200

    inprogress > settings page + tabs checker

---
+++ popup/popup.js
@@ -0,0 +1,12 @@
+const checkServicesBtn = document.getElementById('checkServicesBtn');
+const select1 = document.getElementById('select1');
+const select2 = document.getElementById('select2');
+
+checkServicesBtn.addEventListener('click', function(){
+  if(true){
+    select1.classList.add('ready');
+    select2.classList.add('notReady');
+     
+  }
+})
+
